8328b06a1a032c334f3483f1db69e8cba41c62c5
Eduardo Vicente <eduardo.vicente@inetsys.es>
2021-03-03 10:53:46 +0100

    Horizontal slider with single handler; basic styles

---
+++ demo/Demo.vue
@@ -0,0 +1,107 @@
+<template>
+    <h2>1.- Simple use</h2>
+    <slider
+        v-model="example01.value"
+        :min="example01.min"
+        :max="example01.max"
+    />
+    <div>
+        <button type="button" @click="example01.value = '50'">Set 50</button>
+    </div>
+    <pre>Value: {{ example01.value }} ({{ typeof example01.value }})</pre>
+
+    <h2>2.- Numeric values</h2>
+    <slider
+        v-model.number="example02.value"
+        :min="example02.min"
+        :max="example02.max"
+    />
+    <pre>Value: {{ example02.value }} ({{ typeof example02.value }})</pre>
+
+    <h2>3.- Disable slider</h2>
+    <slider
+        v-model="example03.value"
+        :min="example03.min"
+        :max="example03.max"
+        :disabled="example03.disabled"
+    />
+    <div>
+        <label>
+            <input type="checkbox" v-model="example03.disabled">
+            Disable
+        </label>
+    </div>
+    <pre>Value: {{ example03.value }}</pre>
+
+    <h2>4.- Step</h2>
+    <slider
+        v-model.number="example04.value"
+        :min="example04.min"
+        :max="example04.max"
+        :step="example04.step"
+    />
+    <pre>Value: {{ example04.value }}</pre>
+
+    <h2>5.- Tooltip</h2>
+    <slider
+        v-model="example05.value"
+        :min="example05.min"
+        :max="example05.max"
+        show-value
+    />
+    <pre>Value: {{ example05.value }}</pre>
+</template>
+
+<script>
+import Slider from '@/main.js'
+
+export default {
+    name: 'Demo',
+    components: {
+        Slider,
+    },
+    data() {
+        return {
+            example01: {
+                value: null,
+                min: 0,
+                max: 100,
+            },
+            example02: {
+                value: null,
+                min: 0,
+                max: 100,
+            },
+            example03: {
+                value: 25,
+                min: 0,
+                max: 100,
+                disabled: true,
+            },
+            example04: {
+                value: 3,
+                min: 0,
+                max: 10,
+                step: 1,
+            },
+            example05: {
+                value: 5,
+                min: 0,
+                max: 10,
+                step: 1,
+            },
+        }
+    },
+}
+</script>
+
+<style lang="less">
+#demo {
+    > * {
+        width: 600px;
+        max-width: 95%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+}
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,251 @@
+<template>
+    <div class="slider" :class="containerClasses">
+        <div ref="sliderEl" />
+    </div>
+</template>
+
+<script>
+import { ref, onMounted, watch, computed } from 'vue'
+import nouislider from 'nouislider'
+
+function toggleDisabled(element, disable) {
+    if (disable) {
+        element.setAttribute('disabled', true)
+    }
+    else if (element.hasAttribute('disabled')) {
+        element.removeAttribute('disabled')
+    }
+}
+
+export default {
+    inheritAttrs: false,
+    props: {
+        min: {
+            type: Number,
+            required: true,
+        },
+        max: {
+            type: Number,
+            required: true,
+        },
+        step: {
+            type: Number,
+            default: null,
+        },
+        modelValue: {
+            type: [String, Number],
+            default: null,
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        showValue: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    setup(props, context) {
+        const sliderEl = ref(null)
+        const slider = ref(null)
+
+        onMounted(() => {
+            const initialValue = props.modelValue !== null ? props.modelValue : props.min
+            if (props.modelValue === null) {
+                context.emit('update:modelValue', initialValue.toFixed(2))
+            }
+
+            const sliderOptions = {
+                cssPrefix: 'slider--',
+                start: initialValue,
+                range: {
+                    min: props.min,
+                    max: props.max,
+                },
+                connect: [true, true],
+                tooltips: props.showValue,
+            }
+            if (props.step !== null) {
+                sliderOptions.step = props.step
+            }
+
+            slider.value = nouislider.create(sliderEl.value, sliderOptions)
+            slider.value.on('change', function(values, handle, unencoded, tap, positions, noUiSlider) {
+                context.emit('update:modelValue', values[0])
+            })
+
+            watch(() => props.disabled, (newValue) => toggleDisabled(slider.value.target, newValue), {
+                immediate: true,
+            })
+            watch(() => props.modelValue, newValue => {
+                if (slider.value.get() !== newValue) {
+                    slider.value.set(newValue)
+                }
+            })
+        })
+
+        const containerClasses = computed(() => {
+            const classes = []
+            props.disabled && classes.push('slider--disabled')
+            props.showValue && classes.push('slider--shown-value')
+            props.min === props.modelValue && classes.push('slider--min')
+            props.max === props.modelValue && classes.push('slider--max')
+
+            return classes
+        })
+
+        return { sliderEl, slider, containerClasses }
+    },
+}
+</script>
+
+<style lang="less">
+// Based on nouislider styles - 14.6.3
+
+// Sizes
+@sliderHeight: 20px;
+@sliderHandleSize: 30px;
+// Colors
+@sliderBackgroundColor: #FAFAFA;
+@sliderBorderColor: #D9D9D9;
+@sliderConnectBackgroundColor: #3FB8AF;
+@sliderHandleBackgroundColor: white;
+@sliderDisabledBackgroundColor: #B8B8B8;
+
+.slider {
+    /* Functional styling;
+     * These styles are required for noUiSlider to function.
+     * You don't need to change these rules to apply your design.
+     */
+    &--target,
+    &--target * {
+        -webkit-touch-callout: none;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        -webkit-user-select: none;
+        -ms-touch-action: none;
+        touch-action: none;
+        -ms-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+    &--target {
+        position: relative;
+    }
+    &--base,
+    &--connects {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        z-index: 1;
+    }
+
+    /* Wrapper for all connect elements.
+    */
+    &--connects {
+        overflow: hidden;
+        z-index: 0;
+    }
+    &--connect,
+    &--origin {
+        will-change: transform;
+        position: absolute;
+        z-index: 1;
+        top: 0;
+        right: 0;
+        -ms-transform-origin: 0 0;
+        -webkit-transform-origin: 0 0;
+        -webkit-transform-style: preserve-3d;
+        transform-origin: 0 0;
+        transform-style: flat;
+    }
+    &--connect {
+        height: 100%;
+        width: 100%;
+    }
+    &--origin {
+        height: 10%;
+        width: 10%;
+    }
+
+    /* Give origins 0 height/width so they don't interfere with clicking the
+    * connect elements.
+    */
+    &--horizontal &--origin {
+        height: 0;
+    }
+    &--handle {
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        position: absolute;
+    }
+    &--touch-area {
+        height: 100%;
+        width: 100%;
+    }
+    &--state-tap &--connect,
+    &--state-tap &--origin {
+        -webkit-transition: transform 0.3s;
+        transition: transform 0.3s;
+    }
+    &--state-drag * {
+        cursor: inherit !important;
+    }
+
+    /* Styling;
+    * Giving the connect element a border radius causes issues with using transform: scale
+    */
+    &--target {
+        height: @sliderHeight;
+        background: @sliderBackgroundColor;
+        border-radius: 4px;
+        border: 1px solid #D3D3D3;
+    }
+    &--connects {
+        border-radius: 3px;
+    }
+    &--connect {
+        background: @sliderConnectBackgroundColor;
+    }
+
+    &--handle {
+        width: @sliderHandleSize;
+        height: @sliderHandleSize;
+        right: -@sliderHandleSize / 2;
+        top: -(@sliderHandleSize - @sliderHeight) / 2;
+        border: 1px solid @sliderBorderColor;
+        border-radius: @sliderHandleSize / 2;
+        background: @sliderHandleBackgroundColor;
+        cursor: e-resize;
+
+        &:focus {
+            box-shadow: 0 0 0 1px black;
+            outline: none;
+        }
+    }
+
+    &--tooltip {
+        display: block;
+        position: absolute;
+        border: 1px solid @sliderBorderColor;
+        border-radius: 3px;
+        padding: 5px;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-transform: translate(-50%, 0);
+        transform: translate(-50%, 0);
+        left: 50%;
+        bottom: 120%;
+    }
+
+    [disabled] &--connect {
+        background: @sliderDisabledBackgroundColor;
+    }
+    [disabled]&--target,
+    [disabled]&--handle,
+    [disabled] &--handle {
+        cursor: not-allowed;
+    }
+}
+</style>
